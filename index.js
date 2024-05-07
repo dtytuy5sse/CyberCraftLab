0c80b8764e3c6977ef29b8b8f52142ac9eb30b88
dtytuy5sse <esjcrksahinberk@hotmail.com>
2024-05-07 08:50:44 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const compact = (arr) => arr.filter(Boolean);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
