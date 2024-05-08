4dc3ebdfe4df81e0a0348c60c537f987a3097119
dtytuy5sse <esjcrksahinberk@hotmail.com>
2024-05-08 10:02:21 +0700

    commit

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 1; i <= n + 1; i++) {
-    first = first.next;
-  }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
-  }
-  second.next = second.next.next;
-  return dummy.next;
-}
+const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
